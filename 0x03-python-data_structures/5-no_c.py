2c2027bc4542f244937d75669854437684c5da0b
James gitere <sasmuypwa2019.com>
2023-12-03 07:31:11 +0300

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return None
-
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
             new_string += char
-
     return new_string
