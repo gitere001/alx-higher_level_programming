1b68273bfd145cc906b4420eb72f803c83d90800
James gitere <sasmuypwa2019.com>
2024-01-08 18:28:29 +0300

    BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A mondule containg BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry"""
+    def area(self):
+        """area() is not implemented"""
+        raise Exception('area() is not implemented')
