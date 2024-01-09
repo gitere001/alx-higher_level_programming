4a10d6d1a136252d8cacc1a7470aaa819d193fd1
James gitere <sasmuypwa2019.com>
2024-01-09 13:40:51 +0300

    def read_file(filename=""):

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Module having a function read_file"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout:"""
+    try:
+        with open(filename, 'r', encoding='UTF8') as file:
+            for line in file:
+                print(line, end='')
+    except Exception as e:
+        print(f"Error: {e}")
