e662ad694283b7481c2b8103a7bd146882824c9e
James gitere <sasmuypwa2019.com>
2024-03-11 13:44:01 +0300

    process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 if (process.argv.length === 2){
 	console.log('No argument')
 }
-if (process.argv.length === 3){
+else if (process.argv.length === 3){
 	console.log('Argument found')
 }
-if (process.argv.length === 4){
+else (process.argv.length === 4){
 	console.log('Arguments found')
 }
