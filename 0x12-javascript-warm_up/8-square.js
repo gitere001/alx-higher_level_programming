55b1a0747557b1cdb36893b18be4c0814ea2fb32
James gitere <sasmuypwa2019.com>
2024-03-11 20:45:14 +0300

    7-multi_c.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-
-if (process.argv[2] == undefined || isNaN(process.argv[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 	console.log('Missing size');
-}
-else {
-	const size = parseInt(process.argv[2]);
-	for (let i = 1; i <= size; i++) {
-		console.log('x'.repeat(size));
+  } else {
+	const x = Number(process.argv[2]);
+	let i = 0;
+	while (i < x) {
+	  console.log('X'.repeat(x));
+	  i++;
 	}
-}
+  }
+  
