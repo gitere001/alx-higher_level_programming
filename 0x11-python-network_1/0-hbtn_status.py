ffd640d0ed163f638c81eb7bd2db356e9e7497ae
James gitere <sasmuypwa2019.com>
2024-03-29 12:28:06 +0300

    main block

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as response:
-    body = response.read()
-    print("Body response")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body.decode('utf-8')}")
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
