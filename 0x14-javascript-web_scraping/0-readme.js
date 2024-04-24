4f85ea97f504c17c96fd749e1393fa3b998a8ae9
James gitere <sasmuypwa2019.com>
2024-04-24 15:31:50 +0300

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+const { error } = require('console');
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-	if (err) {
-		console.error(err);
+fs.readFile(process.argv[2], 'utf-8', (err, content) => {
+	if (error) {
+		console.error(error);
 	}
 	else {
 		console.log(content);
