1e1e74654b8f407a5b9ac8e03a74854b0c6da1b4
James gitere <sasmuypwa2019.com>
2024-04-24 14:01:01 +0300

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-
-// Import the 'fs' module to work with the file system
 const fs = require('fs');
-
-// Read the file specified as a command-line argument
 fs.readFile(process.argv[2], 'utf8', function (err, content) {
-    // Check if an error occurred during file reading
-    if (err) {
-        // If an error occurred, log the error to the console
-        console.error('Error reading the file:', err);
-    } else {
-        // If no error occurred, log the file content to the console
-        console.log(content);
-    }
+	if (err) {
+		console.error('Error reading the file:', err);
+	}
+	else {
+		console.log(content);
+	}
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-
-// Import the 'fs' module to work with the file system
 const fs = require('fs');
 
-// Write the specified string to the specified file
-// The file path is provided as the third command-line argument (process.argv[2])
-// The string to write is provided as the fourth command-line argument (process.argv[3])
-// The 'utf8' parameter specifies the encoding of the string (UTF-8)
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
-    // Check if an error occurred during the writing process
     if (error) {
-        // If an error occurred, log the error to the console
         console.error(error);
     }
 });
